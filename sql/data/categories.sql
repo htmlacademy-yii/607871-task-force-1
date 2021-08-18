ff5e8bf59236628684183afcfb3ef143dc561b09
MarkovskayaKsenia <triumh.86@mail.ru>
2021-08-18 16:53:24 +0200

    Yii2. Настройка пустого лейаута

--- sql/data/categories.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO `category` (`id`, `name`, `icon`) VALUES 
-(NULL, 'Переводы', 'translation'), 
-(NULL, 'Уборка', 'clean'), 
-(NULL, 'Переезды', 'cargo'), 
-(NULL, 'Компьютерная помощь', 'neo'), 
-(NULL, 'Ремонт квартирный', 'flat'), 
-(NULL, 'Ремонт техники', 'repair'), 
-(NULL, 'Красота', 'beauty'), 
-(NULL, 'Фото', 'photo');
