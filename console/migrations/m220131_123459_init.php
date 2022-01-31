b37e6f3e0832d4206a506a5a302f0d7b9cea46b9
MarkovskayaKsenia <triumh.86@mail.ru>
2022-01-31 15:16:09 +0200

    Добавление миграции

---
+++ console/migrations/m220131_123459_init.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m220131_123459_init
+ */
+class m220131_123459_init extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $sql = file_get_contents('console/migrations/data/taskmaster_bd.sql');
+        $this->execute($sql);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m220131_123459_init cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m220131_123459_init cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
